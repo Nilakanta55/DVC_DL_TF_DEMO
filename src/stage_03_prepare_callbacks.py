fbdd1c9c872111a731e71b754ea80f461ae3eb3a
nilakanta <nilakanta81@gmail.com>
2021-10-26 14:17:12 +0530

    stage03 updated & completed

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,5 +1,5 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoints_callback
+from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoint_callback
 import argparse
 import os
 import logging
@@ -10,26 +10,28 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
                     filemode="a")
 
-
-def prepare_callbacks(config_path,params_path):
+def prepare_callbacks(config_path, params_path):
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
-    artifatcs = config["artifacts"]
-    artifatcs_dir = artifatcs["ARTIFACTS_DIR"]
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
 
-    tensorboard_log_dir = os.path.join(artifatcs_dir, artifatcs["TENSORBOARD_ROOT_LOG_DIR"])
+    tensorboard_log_dir = os.path.join(artifacts_dir, 
+    artifacts["TENSORBOARD_ROOT_LOG_DIR"])
 
-    checkpoints_dir = os.path.join(artifatcs_dir, artifatcs["CHECKPOINT_DIR"])
+    checkpoint_dir = os.path.join(artifacts_dir, artifacts["CHECKPOINT_DIR"])
+    
+    callbacks_dir = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
 
-    callbacks_dir = os.path.join(artifatcs_dir, artifatcs["CALLBACKS_DIR"])
+    create_directory([
+        tensorboard_log_dir,
+        checkpoint_dir,
+        callbacks_dir
+    ])
 
-    create_directory([tensorboard_log_dir, checkpoints_dir, callbacks_dir])
-
-    create_and_save_tensorboard_callback = (callbacks_dir, tensorboard_log_dir)
-    create_and_save_checkpoints_callback = (callbacks_dir, checkpoints_dir)
-
-
+    create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir)
+    create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir)
 
 
 if __name__ == '__main__':
@@ -43,7 +45,7 @@ if __name__ == '__main__':
     try:
         logging.info(">>>>> stage three started")
         prepare_callbacks(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("stage three completed! callbacks are prepared & saved as binary>>>>>")
+        logging.info("stage three completed! callbacks are prepared and saved as binary>>>>>\n")
     except Exception as e:
         logging.exception(e)
         raise e
