bb90c22966a74013e778c30efaf7c67c38383a69
nilakanta <nilakanta81@gmail.com>
2021-10-26 11:16:54 +0530

    stage 03 base file is ready

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,5 +1,6 @@
 from src.utils.all_utils import read_yaml, create_directory
 from src.utils.model import get_VGG_16_model, prepare_model
+from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoints_callback
 import argparse
 import os
 import logging
@@ -13,7 +14,23 @@ logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=lo
 
 
 def callbacks(config_path,params_path):
-    pass
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    artifatcs = config["artifacts"]
+    artifatcs_dir = artifatcs["ARTIFACTS_DIR"]
+
+    tensorboard_log_dir = os.path.join(artifatcs_dir, artifatcs["TENSORBOARD_ROOT_LOG_DIR"])
+
+    checkpoints_dir = os.path.join(artifatcs_dir, artifatcs["CHECKPOINT_DIR"])
+
+    callbacks_dir = os.path.join(artifatcs_dir, artifatcs["CALLBACKS_DIR"])
+
+    create_directory([tensorboard_log_dir, checkpoints_dir, callbacks_dir])
+
+    create_and_save_tensorboard_callback = (callbacks_dir, tensorboard_log_dir)
+    create_and_save_checkpoints_callback = (callbacks_dir, checkpoints_dir)
+
 
 
 
