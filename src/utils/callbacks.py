f9216e3d53e70ff26d1e98b207b4799b1141f4c9
nilakanta <nilakanta81@gmail.com>
2021-10-27 19:46:43 +0530

    callbacks updated

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -33,13 +33,12 @@ def get_callbacks(callback_dir_path):
 
     callback_path = [
         os.path.join(callback_dir_path, bin_file) for bin_file in os.listdir(callback_dir_path) if bin_file.endswith(".cb")
-
     ]
 
     callbacks = [
         joblib.load(path) for path in callback_path
     ]
 
-    logging.info(f"saved callbacks loaded from {callback_dir_path}")
-    
+    logging.info(f"saved callbacks are loaded from {callback_dir_path}")
+
     return callbacks
