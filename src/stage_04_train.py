94a2eb35fe2f8dace75ce61e82655eb9986a4e50
nilakanta <nilakanta81@gmail.com>
2021-10-27 19:47:29 +0530

    S 04 train and valid data gen created

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,6 +1,7 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.model import load_full_model 
-from src.utils.callbacks import get_callbacks 
+from src.utils.model import load_full_model, get_unique_path_to_save_model
+from src.utils.callbacks import get_callbacks
+from src.utils.data_management import train_valid_generator
 import argparse
 import os
 import logging
@@ -18,7 +19,7 @@ def train_model(config_path, params_path):
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
 
-    train_model_dir_path = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])    
+    train_model_dir_path = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
 
     create_directory([train_model_dir_path])
 
@@ -26,10 +27,36 @@ def train_model(config_path, params_path):
 
     model = load_full_model(untrained_full_model_path)
 
-    callback_dir_path = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+    callback_dir_path  = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
     callbacks = get_callbacks(callback_dir_path)
 
-    
+    train_generator, valid_generator = train_valid_generator(
+        data_dir=artifacts["DATA_DIR"],
+        IMAGE_SIZE=tuple(params["IMAGE_SIZE"][:-1]),
+        BATCH_SIZE=params["BATCH_SIZE"],
+        do_data_augmentation=params["AUGMENTATION"]
+    )
+
+    steps_per_epoch = train_generator.samples // train_generator.batch_size
+    validation_steps = valid_generator.samples // valid_generator.batch_size
+
+    model.fit(
+        train_generator,
+        validation_data=valid_generator,
+        epochs=params["EPOCHS"], 
+        steps_per_epoch=steps_per_epoch, 
+        validation_steps=validation_steps,
+        callbacks=callbacks
+    )
+    logging.info(f"training completed")
+
+    trained_model_dir = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
+    create_directory([trained_model_dir])
+
+    model_file_path = get_unique_path_to_save_model(trained_model_dir)
+    model.save(model_file_path)
+    logging.info(f"trained model is saved at: {model_file_path}")
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
@@ -42,7 +69,7 @@ if __name__ == '__main__':
     try:
         logging.info(">>>>> stage four started")
         train_model(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("stage four completed! training is completed and model is saved>>>>>\n")
+        logging.info("stage four completed! training completed and model is saved >>>>>\n\n")
     except Exception as e:
         logging.exception(e)
         raise e
