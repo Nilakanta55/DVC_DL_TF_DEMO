0dd7f6d09fee421b35b99a59fbc06f1016c1837f
nilakanta <nilakanta81@gmail.com>
2021-10-28 07:50:55 +0530

    S 04 train and valid data gen creat ed & updated

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -37,25 +37,6 @@ def train_model(config_path, params_path):
         do_data_augmentation=params["AUGMENTATION"]
     )
 
-    steps_per_epoch = train_generator.samples // train_generator.batch_size
-    validation_steps = valid_generator.samples // valid_generator.batch_size
-
-    model.fit(
-        train_generator,
-        validation_data=valid_generator,
-        epochs=params["EPOCHS"], 
-        steps_per_epoch=steps_per_epoch, 
-        validation_steps=validation_steps,
-        callbacks=callbacks
-    )
-    logging.info(f"training completed")
-
-    trained_model_dir = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
-    create_directory([trained_model_dir])
-
-    model_file_path = get_unique_path_to_save_model(trained_model_dir)
-    model.save(model_file_path)
-    logging.info(f"trained model is saved at: {model_file_path}")
 
 
 if __name__ == '__main__':
